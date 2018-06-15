b5d29da72dcd9c965bc4db2df87df772e8c49ab5
Anton Lavrenov <lavrton@gmail.com>
2018-06-15 15:27:52 +0700

    update 2018-06-15

---
+++ react-demos/custom-shape/src/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Konva from 'konva';
+import { render } from 'react-dom';
+import { Stage, Layer, Shape } from 'react-konva';
+
+class App extends Component {
+  render() {
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Shape
+            sceneFunc={(context, shape) => {
+              context.beginPath();
+              context.moveTo(20, 50);
+              context.lineTo(220, 80);
+              context.quadraticCurveTo(150, 100, 260, 170);
+              context.closePath();
+              // (!) Konva specific method, it is very important
+              context.fillStrokeShape(shape);
+            }}
+            fill="#00D2FF"
+            stroke="black"
+            strokeWidth={4}
+          />
+        </Layer>
+      </Stage>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
--- source/docs/shapes/Custom.md
+++ source/docs/shapes/Custom.md
@@ -5,7 +5,7 @@ To create a custom shape with Konva, we can instantiate a `Konva.Shape()` object
 
 When creating a custom shape, we need to define a drawing function that is passed a Konva.Canvas renderer.
 
-We can use the renderer to access the HTML5 Canvas context, and to use special methods like `context.fillStrokeShape(this)` which automatically handles filling, stroking, and applying shadows.
+We can use the renderer to access the HTML5 Canvas context, and to use special methods like `context.fillStrokeShape(shape)` which automatically handles filling, stroking, and applying shadows.
 
 For a full list of attributes and methods, check out the [Konva.Shape documentation](https://konvajs.github.io/api/Konva.Shape.html)
 
