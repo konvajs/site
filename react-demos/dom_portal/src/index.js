9e376df157aeb7ed96e8d75d52556f7f6d72d93c
Anton Lavrenov <lavrton@gmail.com>
2018-10-09 08:39:09 -0500

    update 2018-10-09

---
+++ react-demos/dom_portal/src/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Portal from './Portal';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+
+class App extends Component {
+  render() {
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Portal isOpened={true}>
+            <input
+              style={{ position: 'absolute', top: 0, left: 0, width: '200px' }}
+              placeholder="DOM input from Konva nodes"
+            />
+          </Portal>
+          <Rect
+            x={20}
+            y={20}
+            width={50}
+            height={50}
+            fill="red"
+            shadowBlur={5}
+          />
+        </Layer>
+      </Stage>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
