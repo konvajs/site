2ce2765101ea8035a47ef3cd8e498ead092a45c7
Anton Lavrenov <lavrton@gmail.com>
2019-06-27 13:21:04 -0500

    update 2019-06-27

---
+++ react-demos/zIndex/src/index.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import Konva from 'konva';
+import { render } from 'react-dom';
+import { Stage, Layer, Circle } from 'react-konva';
+
+function generateItems() {
+  const items = [];
+  for (let i = 0; i < 10; i++) {
+    items.push({
+      x: Math.random() * window.innerWidth,
+      y: Math.random() * window.innerHeight,
+      id: 'node-' + i,
+      color: Konva.Util.getRandomColor()
+    });
+  }
+  return items;
+}
+
+class App extends Component {
+  state = {
+    items: generateItems()
+  };
+  handleDragStart = e => {
+    const id = e.target.name();
+    const items = this.state.items.slice();
+    const item = items.find(i => i.id === id);
+    const index = items.indexOf(item);
+    // remove from the list:
+    items.splice(index, 1);
+    // add to the top
+    items.push(item);
+    this.setState({
+      items
+    });
+  };
+  onDragEnd = e => {
+    const id = e.target.name();
+    const items = this.state.items.slice();
+    const item = this.state.items.find(i => i.id === id);
+    const index = this.state.items.indexOf(item);
+    // update item position
+    items[index] = {
+      ...item,
+      x: e.target.x(),
+      y: e.target.y()
+    };
+    this.setState({ items });
+  };
+  render() {
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          {this.state.items.map(item => (
+            <Circle
+              key={item.id}
+              name={item.id}
+              draggable
+              x={item.x}
+              y={item.y}
+              fill={item.color}
+              radius={50}
+              onDragStart={this.handleDragStart}
+              onDragEnd={this.handleDragEnd}
+            />
+          ))}
+        </Layer>
+      </Stage>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
---
+++ source/docs/react/zIndex.md
@@ -0,0 +1,18 @@
+title: How to change zIndex of nodes with React?
+layout: react_page
+---
+
+# How to change zIndex and reorder components in `react-konva`?
+
+When you are working with `Konva` directly you have many methods to change order of nodes line `node.zIndex(5)`, `node.moveToTop()`, etc. [Tutorial](/docs/groups_and_layers/Layering.html).
+
+But it is not recommended to use that methods when you are working from the React framework.
+
+`react-konva` is trying to follow the order of the nodes exactly how you described them in `render()`. So instead of changing zIndex manually, you just need to update state of the app correctly, so the `render()` returns correct order.
+
+
+
+<iframe src="https://codesandbox.io/embed/github/konvajs/site/tree/master/react-demos/zIndex?hidenavigation=1&view=split&fontsize=10" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;" sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>
+
+
+
