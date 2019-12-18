0d8e8163e1b23b94c5d427ff4397c637b87efdd7
wujing <wujing@chinacscs.com>
2019-12-18 15:12:58 +0800

    gulpfile升级gulp4配置
    gulp3.x不兼容node12

--- gulpfile.js
+++ gulpfile.js
@@ -9,37 +9,34 @@ var gulp = require('gulp'),
 // 4. generate api docs to /public
 
 // 1 clean /public folder
-gulp.task('clean-public', function() {
-  return gulp.src('public', { read: false }).pipe(clean());
-});
+function cleanPublic() {
+  return gulp.src('public', { read: false, allowEmpty: true }).pipe(clean());
+}
 
 // 2 generate new /public with hexo
-gulp.task(
-  'generate-hexo',
-  ['clean-public'],
-  shell.task(['node ./node_modules/hexo/bin/hexo generate'], { cwd: './' })
-);
+function generateHexo() {
+  return gulp.src('./').pipe(shell(['node ./node_modules/hexo/bin/hexo generate'], { cwd: './' }));
+}
 
 // 3 replace /public/downloads with /source/downloads
-gulp.task('clean-public-downloads', ['generate-hexo'], function() {
+function cleanPublicDownloads() {
   return gulp.src('public/downloads', { read: false }).pipe(clean());
-});
+}
 
-gulp.task('copy-source-download', ['clean-public-downloads'], function() {
+function copySourceDownload() {
   return gulp.src('source/downloads/**').pipe(gulp.dest('public/downloads'));
-});
+}
 
 // 4 generate api docs to /public
-gulp.task('docs', ['clean-public'], function() {
+function generateDocs() {
   var config = require('./jsdoc.json');
   return gulp.src(['./konva.js', '../konva/README.md']).pipe(jsdoc(config));
-});
+}
 
-gulp.task('generate', [
-  'clean-public',
-  'generate-hexo',
-  'copy-source-download',
-  'docs'
-]);
-
-gulp.task('default', ['generate']);
+exports.default = gulp.series(
+  cleanPublic,
+  generateHexo,
+  cleanPublicDownloads,
+  copySourceDownload,
+  generateDocs
+);
