07fddde09b021a5f63daaff4782e9bc3efec2357
Anton Lavrenov <lavrton@gmail.com>
2021-04-03 12:25:29 -0500

    update 2021-04-03

--- react-demos/window-frame-design-app/src/components/RootFrame.js
+++ react-demos/window-frame-design-app/src/components/RootFrame.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Provider } from "mobx-react";
 import { Stage, Layer, Rect } from "react-konva";
 import { observer, inject } from "mobx-react";
 
@@ -17,7 +18,7 @@ class RootFrame extends React.Component {
     });
   }
 
-  handleClick = e => {
+  handleClick = (e) => {
     if (e.target.nodeType === "Stage") {
       this.props.store.selectedSectionId = null;
     }
@@ -31,31 +32,33 @@ class RootFrame extends React.Component {
     const height = padding * 2 + root.height * scale;
     return (
       <div
-        ref={node => {
+        ref={(node) => {
           this.container = node;
         }}
       >
         <Stage
           width={this.state.width}
           height={height}
-          ref={ref => {
+          ref={(ref) => {
             this.stageRef = ref;
           }}
           onClick={this.handleClick}
         >
-          <Layer scaleX={scale} scaleY={scale} y={20} x={20}>
-            <Section
-              section={root.sections[0]}
-              x={root.frameSize}
-              y={root.frameSize}
-            />
-            <Sash
-              width={root.width}
-              height={root.height}
-              size={root.frameSize}
-            />
-            <Metrics />
-          </Layer>
+          <Provider store={window.store}>
+            <Layer scaleX={scale} scaleY={scale} y={20} x={20}>
+              <Section
+                section={root.sections[0]}
+                x={root.frameSize}
+                y={root.frameSize}
+              />
+              <Sash
+                width={root.width}
+                height={root.height}
+                size={root.frameSize}
+              />
+              <Metrics />
+            </Layer>
+          </Provider>
         </Stage>
       </div>
     );
