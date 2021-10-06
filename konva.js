3b77c1b2465de2e75752473ea8ee66ab2e4146b8
Anton Lavrenov <lavrton@gmail.com>
2021-10-06 09:19:58 -0500

    update 2021-10-06

--- konva.js
+++ konva.js
@@ -5,10 +5,10 @@
 })(this, (function () { 'use strict';
 
   /*
-   * Konva JavaScript Framework v8.2.1
+   * Konva JavaScript Framework v8.2.2
    * http://konvajs.org/
    * Licensed under the MIT
-   * Date: Fri Sep 24 2021
+   * Date: Wed Oct 06 2021
    *
    * Original work Copyright (C) 2011 - 2013 by Eric Rowell (KineticJS)
    * Modified work Copyright (C) 2014 - present by Anton Lavrenov (Konva)
@@ -35,7 +35,7 @@
               : {};
   const Konva$2 = {
       _global: glob,
-      version: '8.2.1',
+      version: '8.2.2',
       isBrowser: detectBrowser(),
       isUnminified: /param/.test(function (param) { }.toString()),
       dblClickWindow: 400,
@@ -11060,6 +11060,7 @@
               ctx.lineTo(-length, -width / 2);
               ctx.closePath();
               ctx.restore();
+              this.__fillStroke(ctx);
           }
           if (this.pointerAtBeginning()) {
               ctx.save();
@@ -11079,11 +11080,10 @@
               ctx.lineTo(-length, -width / 2);
               ctx.closePath();
               ctx.restore();
+              this.__fillStroke(ctx);
           }
-          const hasPointer = this.pointerAtEnding() || this.pointerAtBeginning();
-          if (!hasPointer) {
-              return;
-          }
+      }
+      __fillStroke(ctx) {
           // here is a tricky part
           // we need to disable dash for arrow pointers
           var isDashEnabled = this.dashEnabled();
