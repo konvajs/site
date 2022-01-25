40157146eb456f276a32f1e03b92e91b7bad833d
Thierry Kühni <thierry.kuehni@students.fhnw.ch>
2022-01-25 22:07:10 +0100

    updated zIndex example to vue-konva

--- vue-demos/zIndex/src/App.vue
+++ vue-demos/zIndex/src/App.vue
@@ -1,78 +1,67 @@
 <template>
   <div>
-    <v-stage
-      ref="stage"
-      :config="configKonva"
-      @dragstart="handleDragstart"
-      @dragend="handleDragend"
-    >
+    <v-stage ref="stage" :config="configKonva">
       <v-layer ref="layer">
-        <v-star
-          v-for="item in list"
+        <v-circle
+          v-for="item in items"
           :key="item.id"
-          :config="{
-            x: item.x,
-            y: item.y,
-            rotation: item.rotation,
-            id: item.id,
-            numPoints: 5,
-            innerRadius: 30,
-            outerRadius: 50, fill: '#89b717',
-            opacity: 0.8,
-            draggable: true,
-            scaleX: dragItemId === item.id ? item.scale * 1.2 : item.scale,
-            scaleY: dragItemId === item.id ? item.scale * 1.2 : item.scale,
-            shadowColor: 'black',
-            shadowBlur: 10,
-            shadowOffsetX: dragItemId === item.id ? 15 : 5,
-            shadowOffsetY: dragItemId === item.id ? 15 : 5,
-            shadowOpacity: 0.6
-          }"
-        ></v-star>
+          :config="item"
+          @dragstart="handleDragstart"
+          @dragend="handleDragend"
+        ></v-circle>
       </v-layer>
     </v-stage>
   </div>
 </template>
 
 <script>
+import Konva from "konva";
 const width = window.innerWidth;
 const height = window.innerHeight;
+
+function generateItems() {
+  const items = [];
+  for (let i = 0; i < 10; i++) {
+    items.push({
+      x: Math.random() * width,
+      y: Math.random() * height,
+      radius: 50,
+      id: "node-" + i,
+      fill: Konva.Util.getRandomColor(),
+      draggable: true,
+    });
+  }
+  return items;
+}
+
 export default {
   data() {
     return {
-      list: [],
+      items: [],
       dragItemId: null,
       configKonva: {
         width: width,
-        height: height
-      }
+        height: height,
+      },
     };
   },
   methods: {
     handleDragstart(e) {
       // save drag element:
       this.dragItemId = e.target.id();
-      // move current element to the top:
-      const item = this.list.find(i => i.id === this.dragItemId);
-      const index = this.list.indexOf(item);
-      this.list.splice(index, 1);
-      this.list.push(item);
+      // move current element to the top, by rearranging the items array:
+      const item = this.items.find((i) => i.id === this.dragItemId);
+      const index = this.items.indexOf(item);
+      this.items.splice(index, 1);
+      this.items.push(item);
     },
     handleDragend(e) {
       this.dragItemId = null;
-    }
+    },
   },
   mounted() {
-    for (let n = 0; n < 30; n++) {
-      this.list.push({
-        id: Math.round(Math.random() * 10000).toString(),
-        x: Math.random() * width,
-        y: Math.random() * height,
-        rotation: Math.random() * 180,
-        scale: Math.random()
-      });
-    }
-  }
+    this.items = generateItems();
+  },
 };
 </script>
 
